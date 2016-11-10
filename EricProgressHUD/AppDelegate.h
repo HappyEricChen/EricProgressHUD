891e6d9a3fa8fd6e23c39d3de7e881ce59644513
EricChen <chenzhihao.exe@gmail.com>
2016-11-10 16:28:27 +0800

    Initial Commit

---
+++ EricProgressHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EricProgressHUD
+//
+//  Created by eric on 16/11/10.
+//  Copyright © 2016年 eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EricProgressHUD/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  EricProgressHUD
+//
+//  Created by eric on 16/11/10.
+//  Copyright © 2016年 eric. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
